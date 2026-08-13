a27da0981640b284b4ca88a6f08ba79f1e4dca50
Hedi Boukamcha <hedibk264@gmail.com>
2026-08-13 12:20:47 +0100

    adding more mem

--- jobs/scripts/narval_test_xl_one.sh
+++ jobs/scripts/narval_test_xl_one.sh
@@ -25,9 +25,9 @@
 #SBATCH --account=def-adhaj          # <-- A REMPLACER par ton compte RAC (def-xxx ou rrg-xxx)
 #SBATCH --job-name=gnrs-xl-test
 #SBATCH --gpus-per-node=1
-#SBATCH --cpus-per-task=32
-#SBATCH --mem=16G
-#SBATCH --time=00:30:00
+#SBATCH --cpus-per-task=128
+#SBATCH --mem=128G
+#SBATCH --time=24:00:00
 #SBATCH --output=logs/%x-%j.out
 #SBATCH --error=logs/%x-%j.err
 
